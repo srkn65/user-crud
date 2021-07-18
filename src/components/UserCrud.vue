369f8d201c6ba23c39438b2de1a5f4158b424ad1
serkan_korkac <korkacserkan@gmail.com>
2021-07-18 21:01:18 +0300

    user create function added

--- src/components/UserCrud.vue
+++ src/components/UserCrud.vue
@@ -8,7 +8,7 @@
       shadow
     >
       <div class="px-3 py-2">
-        <b-form>
+        <b-form @submit.prevent="addUser()" ref="user-form">
           <b-form-group
             id="firstName"
             class="mt-3"
@@ -19,6 +19,7 @@
               id="firstName"
               placeholder="Enter firstname"
               required
+              v-model="form.firstName"
             ></b-form-input>
           </b-form-group>
           <b-form-group
@@ -29,6 +30,7 @@
           >
             <b-form-input
               id="lastName"
+              v-model="form.lastName"
               placeholder="Enter lastname"
               required
             ></b-form-input>
@@ -36,6 +38,7 @@
           <b-form-group class="mt-3" id="age" label="Age:" label-for="age">
             <b-form-input
               id="age"
+              v-model="form.age"
               placeholder="Enter age"
               required
               type="number"
@@ -49,6 +52,7 @@
           >
             <b-form-input
               id="email"
+              v-model="form.email"
               placeholder="Enter email"
               type="email"
               required
@@ -64,6 +68,7 @@
               id="companyName"
               placeholder="Enter companyname"
               required
+              v-model="form.companyName"
             ></b-form-input>
           </b-form-group>
           <b-button type="submit" variant="success" class="mt-3 w-100" block
@@ -75,7 +80,34 @@
   </div>
 </template>
 <script>
+import { mapMutations } from "vuex";
 export default {
   name: "UserCrud",
+  data() {
+    return {
+      form: {
+        firstName: null,
+        lastName: null,
+        age: null,
+        email: null,
+        companyName: null,
+      },
+    };
+  },
+  methods: {
+    ...mapMutations({
+      setUser: "SET_USER",
+    }),
+    async addUser() {
+      this.setUser(this.form);
+      this.form = {
+        firstName: null,
+        lastName: null,
+        age: null,
+        email: null,
+        companyName: null,
+      };
+    },
+  },
 };
 </script>
