deee676920b90fbfaa3c13442075d5a270bbfce1
serkan_korkac <korkacserkan@gmail.com>
2021-07-18 20:25:27 +0300

    crud table static data added

---
+++ src/components/CrudTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-table :fields="fields" :items="items" striped bordered hover></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          firstName: "serkan",
+          lastName: "korkac",
+          age: 21,
+          email: "korkacserkan@gmail.com",
+          companyName: "X-WİSE",
+        },
+      ],
+      fields: [
+        { key: "firstName", label: "First Name", sortable: false },
+        { key: "lastName", label: "Last Name", sortable: false },
+        { key: "age", label: "Age", sortable: true },
+        { key: "email", label: "Email", sortable: false },
+        { key: "companyName", label: "Şirket ismi", sortable: false },
+      ],
+    };
+  },
+};
+</script>
